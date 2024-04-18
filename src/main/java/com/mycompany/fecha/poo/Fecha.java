170c4fbd0d341709fd410165bcc55b3791f4cb13
juansegarod <juansega96@gmail.com>
2024-04-18 10:33:58 -0500

    se adicionan los codigos

--- src/main/java/com/mycompany/fecha/poo/Fecha.java
+++ src/main/java/com/mycompany/fecha/poo/Fecha.java
@@ -1,5 +1,35 @@
 package com.mycompany.fecha.poo;
 
 public class Fecha {
-    // TODO: Aquí va el código de la clase fechas
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Fecha(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    public String fechaCorta() {
+        return dia + "/" + mes + "/" + ano;
+    }
+
+    public boolean validarFecha() {
+
+        
+        
+        
+        
+        return true;
+    }
+
+    public String mesLetra() {
+        String[] meses = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+        return meses[mes - 1];
+    }
+
+    public String fechaLarga() {
+        return dia + " de " + mesLetra() + " de " + ano;
+    }
 }
